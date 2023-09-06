fcda2e9cad19691d03a6e063afe72910ef9c01f8
Cuong <118752652+cuong-tn@users.noreply.github.com>
2023-09-06 23:36:08 +0700

    Update information.php

--- view/information.php
+++ view/information.php
@@ -1,1 +1,66 @@
-<p class="mt-4 p-4 text-5xl text-[#F54748] bg-[#FFC0CB] rounded-md">THÔNG TIN WEBSITE</p>
+<style>
+        body {
+    font-family: 'Roboto', sans-serif;
+    background-color: rgba(145, 144, 148, 0.16);
+}
+
+.gioithieu h1{
+     margin-left: 50px;
+        }
+.gioithieu div p{
+     width: 100%;
+     text-align: justify;
+     margin: auto;
+     margin-left: 150px;
+     padding: 3px;
+     font-size: 16px;
+     color: black;
+        }
+/* ... các CSS khác ... */
+
+/* Footer CSS */
+
+     </style>
+
+<body>
+    <!-- <img src="../asset/images/logo.webp" alt=""> -->
+   
+
+        </header>
+        <div class="gioithieu">
+            <h1> <a href="index.html" class="flex space-x-2 items-center">
+                <span style="color: white;">Trang Chủ/</span>
+                <span style="color: pink;">Giới thiệu</span>
+            </a>
+        </h1><br>
+           <div>
+            <p>
+             GENCE là thương hiệu phụ kiện đồ da công sở cao cấp lấy điểm nhấn từ sự sang trọng, lịch sự hết sức gần gũi với vóc dáng của người Việt Nam.<br>
+             Những sản phẩm cặp da, túi da, clutch… được chính bàn tay, khối óc của người thợ Việt Nam có tay nghề cao, tâm huyết, tỉ mỉ trong từng công đoạn sản xuất.<br>
+             Kỹ thuật dựng form dáng và sản xuất theo đúng trình tự tiêu chuẩn vô cùng chặt chẽ, chuyên nghiệp để tạo ra những sản phẩm “Made in Viet Nam” chất lượng.
+            </p><br>
+           <p>
+            Với GENCE, chất liệu da được chọn lựa kỹ càng và kỹ thuật cắt may tỉ mỉ, mang lại sự bền bỉ và đẳng cấp cho từng sản phẩm.<br>
+            Những chi tiết tinh tế và tỉ mỉ được thể hiện qua từng đường chỉ và thiết kế độc đáo, tạo nên những phụ kiện đồ da công sở đẳng cấp và đầy cá tính.<br>
+            Hãy để GENCE là người bạn đồng hành tin cậy trong hành trình công việc và gặp gỡ thành công.
+           </p>
+           <br>
+           <p>
+            Chúng tôi cam kết sử dụng các vật liệu chất lượng cao nhất, kết hợp với công nghệ tiên tiến và nguyên tắc thiết kế không thời hạn để tạo ra những<br>
+             sản phẩm thời trang vượt thời gian. Sự quan tâm đến từng chi tiết nhỏ trong quá trình sản xuất cũng giúp chúng tôi đảm bảo rằng mỗi sản phẩm từ <br>
+            GENCE đều phản ánh tinh thần đẳng cấp và chất lượng.
+           </p>
+           <br>
+           <p>
+            Bằng việc chú trọng vào chất lượng sản phẩm, thiết kế đẹp mắt, tinh tế cùng với dịch vụ chăm sóc khách hàng chu đáo,<br>
+            GENCE đã và đang trở thành thương hiệu uy tín, được nhiều người tin dùng trong và ngoài nước.
+           </p>
+           <br>
+           <p>
+            Hãy đến với GENCE, nơi chúng tôi giúp biến không gian làm việc của bạn thành một nơi thực sự thu hút và hiệu quả!"
+           </p>
+    </div>
+        </div>
+</body>
+</html>
+
